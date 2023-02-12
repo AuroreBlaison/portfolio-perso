290eda82517647d6ef3fc19ef2530f81a4aa9b27
Aurore Blaison <aurore_blaison@hotmail.fr>
2023-02-12 17:18:55 +0100

    correction bug suite a la suppression d'une image

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import motif from '../img/motif.jpg'
 
 const Cart = () => {
 
     // besoin d'un state projet sur le meme modele que le state competences pour generer les card via un .map
   return (
     <div className="cart">
-        {/* <img src={motif} alt="" /> */}
+
         <div className="description">
             <h2>Section en Construction</h2> 
         </div>
